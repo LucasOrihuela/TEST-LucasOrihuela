38383e1625107b781221211ea306dd94fce1da40
LucasOrihuela <70446346+LucasOrihuela@users.noreply.github.com>
2024-07-20 13:27:18 -0300

    Add files via upload

---
+++ Acudir.Test.Apis/Controllers/PersonasController.cs
@@ -0,0 +1,40 @@
+﻿using Acudir.Test.Domain;
+using Acudir.Test.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Acudir.Test.Apis.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonasController : ControllerBase
+    {
+        private readonly IPersonaRepository _personaRepository;
+
+        public PersonasController(IPersonaRepository personaRepository)
+        {
+            _personaRepository = personaRepository;
+        }
+
+        [HttpGet("GetAll")]
+        public ActionResult<IEnumerable<Persona>> GetAll([FromQuery] string nombre, [FromQuery] int? edad)
+        {
+            var personas = _personaRepository.GetAll(nombre, edad);
+            return Ok(personas);
+        }
+
+        [HttpPost]
+        public ActionResult Add(Persona persona)
+        {
+            _personaRepository.Add(persona);
+            return Ok();
+        }
+
+        [HttpPut]
+        public ActionResult Update(Persona persona)
+        {
+            _personaRepository.Update(persona);
+            return Ok();
+        }
+    }
+}
